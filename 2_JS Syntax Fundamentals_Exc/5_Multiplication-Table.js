b4853ea979c7f825d8a7497e9b5e16736ba6a6d7
EmiAba <106099092+EmiAba@users.noreply.github.com>
2024-02-08 13:33:33 +0200

    Update 5_Multiplication-Table.js

--- 2_JS Syntax Fundamentals_Exc/5_Multiplication-Table.js
+++ 2_JS Syntax Fundamentals_Exc/5_Multiplication-Table.js
@@ -1,7 +1,19 @@
-function solve(input){
+function solve(num){
+
+ let result=0;
+
+ for (let index = 1; index <=10; index++) {
+   
+    result=index*num;
 
 
+    console.log(`${num} X ${index} = ${result}`)
+    
+ }
 
 
     
 }
+
+
+solve('5')
