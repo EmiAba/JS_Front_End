c1f20c7fad9f7971a048db898a8b4bfe9a0836cb
EmiAba <abadjieva@gmail.com>
2024-03-09 12:39:50 +0200

    Create 2_Math_Power.js

---
+++ 6_Functions_Lab/2_Math_Power.js
@@ -0,0 +1,17 @@
+function solve(input, power){
+    result=MathPower(input, power);
+    console.log(result);
+
+
+
+    function MathPower(input, power){
+
+        let calculation = Math.pow(input, power);
+return calculation;
+
+
+
+
+    }
+}
+solve(2,8)
