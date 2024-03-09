bf475dd6ce405e46253198d8f1be2cb3fc7b13c8
EmiAba <abadjieva@gmail.com>
2024-03-09 11:39:28 +0200

    Create Arrays.js

---
+++ 4_Arrays_and_Strings_Lab/Arrays.js
@@ -0,0 +1,26 @@
+//Declare any array
+const numbers=[1,2, 3,4,5,6];
+let names=['Stoyan', 'Pesho', 'Gosho'];
+
+//Declare an empty array
+let empty=[];//truthy value
+
+//change array by reference
+numbers[0]=10; //kogato e const se smeni 1 elemn ot array; ili smenqne PO REferncia
+console.log(numbers);
+
+//numbers=[2,3,4];//ako e drug masiv she grumne smenqme referenciqta
+
+
+//Dynamically add an element to array
+console.log(empty);
+empty[0]=1;
+empty[1]=2;
+
+console.log(empty);
+
+//
+
+
+
+
