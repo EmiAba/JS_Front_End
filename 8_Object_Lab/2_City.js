f77c4b686a30bcf8dd47793d9cba92205865ddef
EmiAba <abadjieva@gmail.com>
2024-03-11 19:05:12 +0200

    Create 2_City.js

---
+++ 8_Object_Lab/2_City.js
@@ -0,0 +1,12 @@
+function solve(name, area, population, country, postcode){
+    let city={
+        name: name, 
+        area: area, 
+        population:population, 
+        country:country, 
+        postcode:postcode
+    }
+
+    console.log(`name -> ${city.name}`);
+
+}
