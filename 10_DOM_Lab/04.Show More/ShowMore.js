9a293e1002d8cc32d7facd9e9b26f8803ec80627
EmiAba <abadjieva@gmail.com>
2024-03-18 21:49:58 +0200

    Create ShowMore.js

---
+++ 10_DOM_Lab/04.Show More/ShowMore.js
@@ -0,0 +1,9 @@
+function showText() {
+   const moreButton=document.getElementById('more');
+const hiddenText=document.getElementById('text');
+
+moreButton.style.display='none';
+hiddenText.style.display='inline';
+
+
+}
