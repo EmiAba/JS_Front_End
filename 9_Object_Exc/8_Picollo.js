35aefaaf1846806ae0f011593fb50c7a15596abd
EmiAba <abadjieva@gmail.com>
2024-03-24 11:23:02 +0200

    Update 8_Picollo.js

--- 9_Object_Exc/8_Picollo.js
+++ 9_Object_Exc/8_Picollo.js
@@ -18,8 +18,50 @@ function trackParkingLotOperations(operations) {
     }
 }
 
+function setSolve(input){
+    const parking=new Set();
+
+for (const row of input) {
+     const[direction, carNumber]= row.split(', ');
+     direction==='IN' 
+     ? parking.add(carNumber)
+     : parking.delete(carNumber)
+    }
+if(parking.size<1){
+    console.log(`Parking Lot is Empty`);
+    return;
+}
+Array.from(parking.values())
+.sort((a,b)=> a.localeCompare(b))
+.forEach(car=>console.log(car));
+
+}
+
+function solve(input){
+    const parkingLot={};
+
+    for (const row of input) {
+        const [direction, carNumber]= row.split(', ');
+
+     direction==='IN'
+            ? parkingLot[carNumber]=true
+            : delete parkingLot[carNumber]
+
+
+        
+    }
+  
+
+Object.keys(parkingLot)
+.sort((a,b)=> a.localeCompare(b))
+.forEach(carNumber=>console.log(carNumber));
+
+}
+
+
+
 // Example usage:
-trackParkingLotOperations([
+solve([
     'IN, CA2844AA',
     'IN, CA1234TA',
     'OUT, CA2844AA',
