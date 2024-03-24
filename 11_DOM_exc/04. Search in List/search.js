6f918e7d4ee8bc9984e6995b60c2c00257c7e7cd
EmiAba <abadjieva@gmail.com>
2024-03-24 12:08:15 +0200

    Update search.js

--- 11_DOM_exc/04. Search in List/search.js
+++ 11_DOM_exc/04. Search in List/search.js
@@ -8,13 +8,16 @@ const searchText=searchTextElement.value;
 
 let matchCount=0;
 
-for (const townElement of townListElement.children) {
+
+const townElements=Array.from(townListElement.children);
+for (const townElement of townElements) {
  if(townElement.textContent.toLowerCase().includes(searchText.toLowerCase())){
    townElement.style.textDecoration='underline';
    townElement.style.fontWeight='bold';
 
    matchCount++;
  }
+
 resultElement.textContent=`${matchCount} matches found`;
    
 }
