275a7b6ab17c4c6cf090e70994c9804aadf26615
EmiAba <abadjieva@gmail.com>
2024-03-25 12:12:21 +0200

    Create solution.js

---
+++ 11_DOM_exc/06. Format the Text/solution.js
@@ -0,0 +1,24 @@
+function solve() {
+  const outputElement = document.getElementById("output");
+  const textAreaElement = document.getElementById("input");
+  const text = textAreaElement.value;
+
+  //convert to paragraphs
+  const result = text
+    .split(".")
+    .reduce((result, sentence, i) => {
+      const resultIndex = Math.floor(i / 3);
+      if (!result[resultIndex]) {
+        result[resultIndex] = [];
+      }
+
+      result[resultIndex].push(sentence.trim());
+      return result;
+    }, [])
+    .map((sentences) => `<p>${sentences.join('. ')}.</p>`)
+    .join('\n');
+
+  // console.log(result)
+  //append to output elemnts
+  outputElement.innerHTML=result;
+}
