ccbb59ccf2219c49bc014ec199bd1b5234cc04f6
EmiAba <abadjieva@gmail.com>
2024-03-25 19:35:19 +0200

    Create SumTable.js

---
+++ 10_DOM_Lab/06.Sum Table/SumTable.js
@@ -0,0 +1,8 @@
+function sumTable() {
+
+const  tdCostElements=document.querySelectorAll('tr td:last-of-type:not(#sum)');
+const tdSumElement=document.getElementById('sum');
+
+ const sum=Array.from(tdCostElements)
+ .reduce((sum,cost)=>sum+Number(element.textContent), 0);
+}
