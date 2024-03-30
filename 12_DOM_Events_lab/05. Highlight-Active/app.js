f2b7d25f3c2bf585ee642a5d0d0429ac0eb9e0a5
EmiAba <abadjieva@gmail.com>
2024-03-30 11:59:29 +0200

    Update app.js

--- 12_DOM_Events_lab/05. Highlight-Active/app.js
+++ 12_DOM_Events_lab/05. Highlight-Active/app.js
@@ -1,3 +1,22 @@
 function focused() {
-    console.log('TODO:...');
+    const inputElements=document.querySelectorAll('input[type=text]');
+
+     inputElements
+     .forEach(inputElem => inputElem.addEventListener('focus', (e)=>{
+      // console.log(e.target.parentElement);
+      e.target.parentElement.classList.add('focused');
+
+
+     }
+));
+
+inputElements
+.forEach(inputElem => inputElem.addEventListener('blur', (e)=>{
+ 
+ e.target.parentElement.classList.remove('focused');
+
+
+}
+));
+    
 }
