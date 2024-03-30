39e2b89f192d724ceef7bcf802b293e012e759db
EmiAba <abadjieva@gmail.com>
2024-03-30 11:24:04 +0200

    Update app.js

--- 12_DOM_Events_lab/04. Mouse-Gradient/app.js
+++ 12_DOM_Events_lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,23 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradientElement=document.getElementById('gradient');
+        const resultElement=document.getElementById('result');
+
+
+        gradientElement.addEventListener('mousemove', (event)=>{
+                //console.log(`event`);
+
+        const currentWidth=event.offsetX; //start
+        const elementWidth=event.target.clientWidth;
+
+        const progress=Math.floor((currentWidth / elementWidth)*100);
+
+resultElement.textContent=`${progress}%`;
+
+
+
+        })
+
+
+
+
 }
