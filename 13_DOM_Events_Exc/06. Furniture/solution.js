05a31993d5c19e4fb6bac7ff21a30b9facac5c49
EmiAba <abadjieva@gmail.com>
2024-04-03 10:27:33 +0300

    Update solution.js

--- 13_DOM_Events_Exc/06. Furniture/solution.js
+++ 13_DOM_Events_Exc/06. Furniture/solution.js
@@ -1,4 +1,60 @@
 function solve() {
+const textareaInputElement = document.querySelector('#exercise textarea:first-of-type');
+const textareaOutputElement= document.querySelector('#exercise textarea:last-of-type');
+const generateButtonElement= document.querySelector('#exercise button:first-of-type');
+const buyButtonElement= document.querySelector('#exercise button:last-of-type');
+const furnitureTbodyElement = document.querySelector('.table tbody');
 
-  //TODO...
+
+//Parse input 
+generateButtonElement.addEventListener('click',(e) =>{
+  const funitures=JSON.parse(textareaInputElement.value);
+
+  for (const furniture of funitures) {
+   
+    const imgElement = document.createElement('img');
+    imgElement.src = furniture.img;
+    const imageTdElement = document.createElement('td');
+    imageTdElement.appendChild(imgElement);
+
+    const namePElement = document.createElement('p');
+    namePElement.textContent = furniture.name;
+    const nameTdElement = document.createElement('td');
+    nameTdElement.appendChild(namePElement);
+
+    const pricePElement = document.createElement('p');
+    pricePElement.textContent = furniture.price;
+    const priceTdElement = document.createElement('td');
+    priceTdElement.appendChild(pricePElement);
+
+    const decPElement = document.createElement('p');
+    decPElement.textContent = furniture.decFactor;
+    const decTdElement = document.createElement('td');
+    decTdElement.appendChild(decPElement);
+
+    const markElement = document.createElement('input');
+    markElement.setAttribute('type', 'checkbox');
+    const markTdElement = document.createElement('td');
+    markTdElement.appendChild(markElement);
+
+    
+  }
+
+
+
+}
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
