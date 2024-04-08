9cbec8c6ba8f9df6c89bd6e3b6db8b687e9d9ee6
EmiAba <abadjieva@gmail.com>
2024-04-08 19:12:04 +0300

    Update SumTable.js

--- 10_DOM_Lab/06.Sum Table/SumTable.js
+++ 10_DOM_Lab/06.Sum Table/SumTable.js
@@ -1,8 +1,22 @@
 function sumTable() {
+    let table = document.querySelector('table')
+    let lastColumns = Array.from(document.querySelectorAll('td:nth-child(even):not(#sum)'))
+    let sumColumnElement = document.getElementById('sum')
 
-const  tdCostElements=document.querySelectorAll('tr td:last-of-type:not(#sum)');
-const tdSumElement=document.getElementById('sum');
+    let totalSum = 0
+    for (let element of lastColumns) {
+        totalSum += Number(element.textContent)
+    }
 
- const sum=Array.from(tdCostElements)
- .reduce((sum,cost)=>sum+Number(element.textContent), 0);
+    sumColumnElement.textContent = totalSum
 }
+
+
+// function sumTable() {
+
+// const  tdCostElements=document.querySelectorAll('tr td:last-of-type:not(#sum)');
+// const tdSumElement=document.getElementById('sum');
+
+//  const sum=Array.from(tdCostElements)
+//  .reduce((sum,cost)=>sum+Number(element.textContent), 0);
+// }
