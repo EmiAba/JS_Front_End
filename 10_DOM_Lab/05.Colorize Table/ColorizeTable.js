301e4efbd5713d134612e781a22cecf0d76c537c
EmiAba <abadjieva@gmail.com>
2024-04-08 19:11:12 +0300

    Update ColorizeTable.js

--- 10_DOM_Lab/05.Colorize Table/ColorizeTable.js
+++ 10_DOM_Lab/05.Colorize Table/ColorizeTable.js
@@ -1,8 +1,17 @@
 function colorize() {
+    let rowElements = Array.from(document.querySelectorAll('tr:nth-child(even)'))
+
+    for (let element of rowElements) {
+        element.style.backgroundColor = 'Teal'
+    }
+}
+
+
+// function colorize() {
    
-const evenRowElements=document.querySelectorAll('table tr:nth-child(2n');
-evenRowElements.forEach(element=>element.style.backgroundColor='teal');
+// const evenRowElements=document.querySelectorAll('table tr:nth-child(2n');
+// evenRowElements.forEach(element=>element.style.backgroundColor='teal');
 
     
 
-}
+// }
